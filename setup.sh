6c4560b23fd1b86704a6092076dd1077592df519
sunakan <sunakan03@gmail.com>
2024-11-17 09:05:15 +0900

    wezterm: wezterm.luaを追加

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 mkdir -p ~/.config/mise/
 [ ! -f ~/.config/mise/config.toml ] && ln -s $(pwd)/mise/config.toml ~/.config/mise/config.toml
 ls -lh ~/.config/mise/config.toml
+
+mkdir -p ~/.config/wezterm/
+[ ! -f ~/.config/wezterm/wezterm.lua ] && ln -s $(pwd)/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
+ls -lh ~/.config/wezterm/wezterm.lua
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.font = wezterm.font('HackGen Console NF')
+config.font_size = 18.0
+
+config.color_scheme = 'Solarized Darcula'
+
+return config
