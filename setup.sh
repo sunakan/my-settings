2f5ec781830dbfbe79c27cfb5ac213f213223290
sunakan <sunakan03@gmail.com>
2024-11-23 12:35:36 +0900

    lazygit: Cでコミットメッセージをvimで打つ

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@
 
 mkdir -p ~/.config/mise/
 [ ! -f ~/.config/mise/config.toml ] && ln -s $(pwd)/mise/config.toml ~/.config/mise/config.toml
-ls -lh ~/.config/mise/config.toml
 
 mkdir -p ~/.config/wezterm/
 [ ! -f ~/.config/wezterm/wezterm.lua ] && ln -s $(pwd)/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
-ls -lh ~/.config/wezterm/wezterm.lua
+
+mkdir -p ~/.config/lazygit/
+[ ! -f ~/.config/lazygit/config.yml ] && ln -s $(pwd)/.config/lazygit/config.yml ~/.config/lazygit/config.yml
+
+ls -lh ~/.config/**
