c1b890c2c3e0ba6ffb8eaea0b225a23bce5353bb
sunakan <sunakan03@gmail.com>
2024-11-30 09:41:20 +0900

    wezterm: 文字サイズを変更した時、ウィンドウサイズは維持

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,14 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
+-- フォント
 config.font = wezterm.font('HackGen Console NF')
 config.font_size = 18.0
 
+-- フォントサイズ変更時にウィンドウサイズを変更しない
+config.adjust_window_size_when_changing_font_size = false
+
+-- デフォルトで入っているカラースキーマ
 config.color_scheme = 'Solarized Darcula'
 
 return config
