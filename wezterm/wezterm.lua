30c76f40eeb39a4fadae0fb10c3aafbe33de4131
sunakan <sunakan03@gmail.com>
2026-05-15 20:59:47 +0900

    chore: wezterm のビープ音を無効化してビジュアルベルを追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,4 +15,16 @@ config.adjust_window_size_when_changing_font_size = false
 -- config.color_scheme = "Github Dark (Gogh)"
 config.color_scheme = "Gogh (Gogh)"
 
+-- ビープ無効化 + ビジュアルベル
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 10,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 500,
+}
+config.colors = {
+  visual_bell = '444411',
+}
+
 return config
